69b6f86c76d91c83cf4957b720fa071a4b9ac231
John C <113877403+Solved-Omega@users.noreply.github.com>
2023-07-13 11:15:55 -0700

    Removed Project Files

--- EAESFieldLabVR/Source/EAESFieldLabVR/ExportTXT.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "ExportTXT.h"
-
-bool UExportTXT::WriteTXT(FString Text, FString FileName)
-{
-	return FFileHelper::SaveStringToFile(Text, *(FPaths::ProjectDir() + FileName));
-}
